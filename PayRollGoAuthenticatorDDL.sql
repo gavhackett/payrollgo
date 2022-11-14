e4447c5ec2a813c4031203fd6a685d2a228170c4
Bryan Dunn <76450960+ImSoDunn@users.noreply.github.com>
2022-11-13 18:18:02 -0600

    Delete PayRollGoAuthenticatorDDL.sql

--- PayRollGoAuthenticatorDDL.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE PayRollGoAuthentication 
-(
-	employeeID BIGINT NOT NULL,
-    userName VARCHAR(20),
-    userPassword VARCHAR(20) AS (SHA2(employeeID, 256)) STORED NOT NULL,
-    PRIMARY KEY (employeeID)
-);
