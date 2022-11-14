359055a5b356712ede7baaa1a4bd5a7b79fd8c16
Bryan Dunn <76450960+ImSoDunn@users.noreply.github.com>
2022-11-13 18:17:06 -0600

    Delete BankAccountDDL.sql
    Deleting for Organizational purposes.

--- BankAccountDDL.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE BankAccount
-(
-	employeeID BIGINT NOT NULL,
-    balance DEC(8,2) NOT NULL,
-    nameBank VARCHAR(50) NOT NULL,
-    routingNumber CHAR(9),
-    PRIMARY KEY (employeeID),
-    FOREIGN KEY (employeeID) REFERENCES Employee(employeeID)
-);
