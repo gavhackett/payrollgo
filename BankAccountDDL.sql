c45cfdeef39b3f11632803fe82373cc1d95626fb
gavhackett <109842510+gavhackett@users.noreply.github.com>
2022-11-19 23:48:57 +0000

    Editing sql files for database testing

--- BankAccountDDL.sql
+++ BankAccountDDL.sql
@@ -1,9 +1,9 @@
 CREATE TABLE BankAccount
 (
-	employeeID BIGINT NOT NULL,
+	employeeID BIGINT NOT NULL AUTO_INCREMENT,
     balance DEC(8,2) NOT NULL,
     nameBank VARCHAR(50) NOT NULL,
-    routingNumber CHAR(9),
+    routingNumber CHAR(9) NOT NULL,
     PRIMARY KEY (employeeID),
     FOREIGN KEY (employeeID) REFERENCES Employee(employeeID)
 );
--- EmployeeDDL.sql
+++ EmployeeDDL.sql
@@ -1,11 +1,11 @@
 CREATE TABLE Employee 
 (
-	employeeID BIGINT NOT NULL,
-	taxBracket BIT NOT NULL,
+	employeeID BIGINT NOT NULL UNSIGNED AUTO_INCREMENT,
+	taxBracket DECIMAL(3,2) NOT NULL,
 	nameFirst VARCHAR(20) NOT NULL,
 	nameLast VARCHAR(20) NOT NULL,
-	hoursWorked SMALLINT,
-	hourlyWage SMALLINT NOT NULL,
+	hoursWorked SMALLINT NOT NULL UNSIGNED,
+	hourlyWage SMALLINT NOT NULL UNSIGNED,
 	phoneNumber CHAR(12),
 	address VARCHAR(255) NOT NULL,
     CHECK (taxBracket in (0.10,0.12,0.22,0.24,0.32,0.35,0.37)),
